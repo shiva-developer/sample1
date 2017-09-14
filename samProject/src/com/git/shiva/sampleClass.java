59f71381022938100691cd02469ee20dba98950f
shiva-developer <shiva2301.kumar@gmail.com>
2017-09-14 20:10:49 +0530

    Third commit

--- samProject/src/com/git/shiva/sampleClass.java
+++ samProject/src/com/git/shiva/sampleClass.java
@@ -3,6 +3,6 @@ package com.git.shiva;
 public class sampleClass {
 
 	public static void main(String[] args){
-		System.out.println("first git example");
+		System.out.println("third git example");
 	}
 }
